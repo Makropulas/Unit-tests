9b1bdbe2e22445d1682dca2ff338042495ada075
DmitryMalomoshin <makropulas@gmail.com>
2023-12-30 01:06:54 +0300

    Completed dz2

---
+++ src/main/java/dz2/Main.java
@@ -0,0 +1,11 @@
+package dz2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Acura", "MDX", 2015);
+        Motorcycle motorcycle = new Motorcycle("Honda", "CBR 1000", 2014);
+
+        System.out.println(car);
+        System.out.println(motorcycle);
+    }
+}
